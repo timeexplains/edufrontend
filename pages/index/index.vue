fe9bddd5efaf9b386818b1d5caaf847ee62b1156
pzh_1001@163.com <pzh_1001@163.com>
2021-03-21 19:22:12 +0800

    vue study

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="">
+		<mycomponent title="hi" content="content..." @tap1="tap"></mycomponent>
+	</view>
+</template>
+
+<script>
+	import uniNumberBox from "@/components/uni-number-box/uni-number-box.vue"
+	import mycomponent  from "@/components/mycomponent.vue"
+	export default {
+		data() {
+			return {
+				title: 'hello...'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			tap:function(e){
+					console.log(e);
+				}
+		},
+		components:{uniNumberBox,
+		mycomponent}
+	}
+</script>
+
+<style lang="less">
+@import "../../less.less";
+</style>
